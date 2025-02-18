d6a5ecf166f3ce3f544d695e7aa3c530b342d034
Garcia,Rene <rene.orlando.garcia@aptiv.com>
2025-02-18 13:22:33 -0600

    something missing

--- examples/cli-utils/src/colors.rs
+++ examples/cli-utils/src/colors.rs
@@ -15,17 +15,45 @@ pub fn red(s: &str) -> String {
     format!("\x1b[31m{}\x1b[0m", s)
 }
 
+/// Returns a string with ANSI escape code for green.
+/// # Examples:
+/// ```
+/// 
+/// use cli_utils::colors::*;
+/// println!("{}", green("Green"));
+/// ```
 pub fn green(s: &str) -> String {
     format!("\x1b[32m{}\x1b[0m", s)
 }
+/// Returns a string with ANSI escape code for blue.
+/// # Examples:
+/// ```
+/// 
+/// use cli_utils::colors::*;
+/// println!("{}", blue("Blue"));
+/// ```
 
 pub fn blue(s: &str) -> String {
     format!("\x1b[34m{}\x1b[0m", s)
 }
+/// Returns a string with ANSI escape code for bold.
+/// # Examples:
+/// ```
+/// 
+/// use cli_utils::colors::*;
+/// println!("{}", bold("Bold"));
+/// ```
 
 pub fn bold(s: &str) -> String {
     format!("\x1b[1m{}\x1b[0m", s)
 }
+/// Returns a string with ANSI escape code for reset.
+/// # Examples:
+/// ```
+/// 
+/// use cli_utils::colors::*;
+/// println!("{}", reset("Reset"));
+/// ```
 
 pub fn reset(s: &str) -> String {
     format!("\x1b[0m{}\x1b[0m", s)
@@ -38,6 +66,15 @@ pub enum Color{
     Bold,
 }
 
+/// 
+/// `ColorString` holds the properties of a colorized string, including the string itself.
+/// 
+/// # Examples:
+/// You can paint the string with [`ColorString::paint()`] method.
+/// 
+/// ```
+/// let painted_string = ColorString.paint();
+/// ```
 pub struct ColorString {
     pub color: Color,
     pub string: String,
